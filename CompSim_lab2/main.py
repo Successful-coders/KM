69c92da064d41e54774202f4ac1606aac4ac7ada
voronik1801 <divi1801@icloud.com>
2021-10-10 17:15:55 +0700

    fix chi and test2

--- CompSim_lab2/main.py
+++ CompSim_lab2/main.py
@@ -39,13 +39,6 @@ def create_freq(N, param):
         # x[i+1] = (2*x[i] + 3*(x[i-1]**2) + 4) % 100
     return x
 
-def create_plot(x):
-    plot.plot(x)
-    plot.xlabel('x')
-    plot.grid(True, which='both')
-    plot.axhline(y=0, color='k')
-    plot.axvline(x=0, color='k')
-    plot.show()
 
 def calc_period(x):
     x1 = x[999]
@@ -93,15 +86,7 @@ def test1(x, n, param):
         test = True
     else:
         df['Test'] = '-'
-    print(df)
-    fig, ax = plot.subplots()
-    fig.patch.set_visible(False)
-    ax.axis('off')
-    ax.axis('tight')
-    table = ax.table(cellText=df.values, colLabels=df.columns, loc='center')   
-    table.auto_set_font_size(False)
-    table.set_fontsize(8)
-    fig.tight_layout()
+    create_table(df)
     plot.show()
     return test
 
@@ -124,21 +109,19 @@ def test2(x, n, param):
 
     U = scipy.stats.norm.ppf(1 - param['alpha']/2)
     # create dov int
+    df = pd.DataFrame()
     dov_int = np.zeros((int(param['K1']), 2))
     for i in range (int(param['K1'])):
         dov_int[i][0] = kint[0][i] - U * math.sqrt((int(param['K1']) - 1) / n) / int(param['K1'])
         dov_int[i][1] = kint[0][i] + U * math.sqrt((int(param['K1']) - 1) / n) / int(param['K1'])
-        print(np.round((dov_int[i][0]), 5))
-    df = pd.DataFrame()
-    # df['Нижнее знач. дов. интервала'] = [np.round(pd.Series(dov_int[i][0]), 5)  for i in range(int(param['K1']))]
-    # df['Верхнее знач. дов. интервала'] = [np.round(pd.Series(dov_int[i][1]), 5) for i in range(int(param['K1']))]
+    df['_'] = np.round(dov_int[:,0], 5)
+    df['^'] = np.round(dov_int[:,1], 5)
+    df['v'] = kint[0]
 
-    # df['v теоретическое'] = str(np.round(v, 3))
-    print(v)
     result = False
     temp = []
     for i in range(int(param['K1'])):
-        if v > dov_int[i][0] and v < dov_int[i][1]:
+        if kint[0][i] > dov_int[i][0] and kint[0][i] < dov_int[i][1]:
             temp.append('+')
             result = True
         else:
@@ -147,16 +130,19 @@ def test2(x, n, param):
     # print(temp)
 
     df1 = pd.DataFrame()
+    M = mo(x_new)
+    D = disp(x, int(param['m']))
     M_teor = int(param['m']) / 2
-    D_teor = int(param['m']) ** 2 / 12
+    D_teor = (int(param['m']) ** 2) / 12
     df1['M_teor'] = pd.Series(M_teor)
+    df1['M_true'] = pd.Series(M)
     # доверительные интервалы для мат. ожидания
     dov_int_M = np.zeros((2))
     dov_int_M[0] = M - U * math.sqrt(D) / math.sqrt(n)
     dov_int_M[1] = M + U * math.sqrt(D) / math.sqrt(n)
-    df1['Нижнее знач. дов. интервала M'] = np.round(pd.Series(dov_int_M[0]), 5)
-    df1['Верхнее знач. дов. интервала M'] = np.round(pd.Series(dov_int_M[1]), 5)
-    if M_teor > dov_int_M[0] and M_teor < dov_int_M[1]:
+    df1['Нижнее знач.'] = np.round(pd.Series(dov_int_M[0]), 5)
+    df1['Верхнее знач.'] = np.round(pd.Series(dov_int_M[1]), 5)
+    if M > dov_int_M[0] and M < dov_int_M[1]:
         result = True
         df1['M в интервале'] = pd.Series('+')
     else:
@@ -165,13 +151,14 @@ def test2(x, n, param):
 
     df2 = pd.DataFrame()
     df2['D_teor'] = pd.Series(D_teor)
+    df2['D'] = pd.Series(D)
     # доверительные интервалы для дисперсии
     dov_int_D = np.zeros((2))
     dov_int_D[0] = (n - 1) * D / (scipy.stats.chi2.ppf(1 - param['alpha'] / 2, n-1))
     dov_int_D[1] = (n - 1) * D / (scipy.stats.chi2.ppf(param['alpha'] / 2, n-1))
-    df2['Нижнее знач. дов. интервала D'] = np.round(pd.Series(dov_int_D[0]), 5)
-    df2['Верхнее знач. дов. интервала D'] = np.round(pd.Series(dov_int_D[1]), 5)
-    if D_teor > dov_int_D[0] and D_teor < dov_int_D[1]:
+    df2['Нижнее знач.'] = np.round(pd.Series(dov_int_D[0]), 5)
+    df2['Верхнее знач.'] = np.round(pd.Series(dov_int_D[1]), 5)
+    if D > dov_int_D[0] and D < dov_int_D[1]:
         result = True
         df2['D в интервале'] = pd.Series('+')
     else:
@@ -179,13 +166,37 @@ def test2(x, n, param):
         df2['D в интервале'] = pd.Series('-')
     
     print(f'Для n={n}')
-    df.to_csv('KM/CompSim_lab2/out.csv', header=False, index=False)
-    print(df)
-    print(df1)
-    print(df2)
+    create_table(df)
+    create_table(df1)
+    create_table(df2)
     plot.show()
     return result
 
+def create_table(df):
+    fig, ax = plot.subplots()
+    fig.patch.set_visible(False)
+    ax.axis('off')
+    ax.axis('tight')
+    table = ax.table(cellText=df.values, colLabels=df.columns, loc='center')   
+    table.auto_set_font_size(False)
+    table.set_fontsize(8)
+    fig.tight_layout()
+
+# Математическое ожидание
+def mo(x):
+    m = 0
+    for i in range(len(x)):
+        m += x[i]
+    m /= len(x)
+    return m
+
+# Дисперсия
+def disp(x, m):
+    d = 0
+    for i in range(len(x)):
+        d += (x[i] - m) ** 2
+    d /= (len(x) - 1)
+    return d
 
 def test3(x, n, param):
     t = int((n - 1) / param['r'])
@@ -210,14 +221,9 @@ def test3(x, n, param):
     
     if(res1 and res2):
         result = True
-    fig, ax = plot.subplots()
-    fig.patch.set_visible(False)
-    ax.axis('off')
-    ax.axis('tight')
-    table = ax.table(cellText=df.values, colLabels=df.columns, loc='center')   
-    table.auto_set_font_size(False)
-    table.set_fontsize(8)
-    fig.tight_layout()
+
+    create_table(df)
+
     plot.show() 
     return result
 
@@ -238,14 +244,17 @@ def chi_test(x, n, param):
     print(f'Для {n}')
     print(f'S={S}')
     plot.show()
-    ur = scipy.stats.chi2.ppf(1-param['alpha'], param['K1'] - 1)
-    print(f'Уровень значимости = {ur}')
-    if ur > S:
+    
+    p = (1 /(2**(int(param['r'])/2)*math.gamma(int(param['r'])/2)))*scipy.integrate.quad(lambda s:s**((int(param['r'])/2)-i)*np.exp(-s/2), S, np.inf)[0]
+    print(f'Уровень значимости = {p}')
+
+    if p < param['alpha']:
         print('Гипотеза по хи-квадрат не отклоняется')
-        return True #гипотеза по хи-квадрат не отклоняется
+        return False
     else:
         print('Гипотеза по хи-квадрат отклоняется')
         return False #гипотеза по хи-квадрат  отклоняется
+    plot.show()
 
 # функция распределения
 F = lambda x, n: x/ (n - 1) 
@@ -296,7 +305,7 @@ def kolmogorov(x, n, param):
 def main():
     param = convert_param('KM/CompSim_lab2/input.txt')
     N = 1000
-    gen = 2
+    gen = 1
     if (gen == 1):
         x = create_freq(N, param)
     else:
@@ -308,8 +317,8 @@ def main():
         # test1(x, 40, param)
         # test1(x, 100, param)
 
-        test2(x, 40, param)
-        test2(x, 100, param)
+        # test2(x, 40, param)
+        # test2(x, 100, param)
 
         # test3(x, int(40 / int(param['r'])), param)
         # test3(x, int(100 / int(param['r'])), param)
@@ -317,7 +326,7 @@ def main():
         # chi_test(x, 40, param)
         # chi_test(x, 100, param)
 
-        # kolmogorov(x, 40, param)
-        # kolmogorov(x, 100, param)
+        kolmogorov(x, 40, param)
+        kolmogorov(x, 100, param)
 
 main()
