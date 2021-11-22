09f1b96726c0967e90e8462dd64c3f75b16ecb01
voronik1801 <divi1801@icloud.com>
2021-11-22 12:01:49 +0700

    lab 5

--- CompSim_lab4/main.py
+++ CompSim_lab4/main.py
@@ -1,9 +1,13 @@
 import numpy as np
 import math
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from datetime import datetime
 import random
-import scipy.stats
+import scipy.integrate as integrate
+from scipy.special import gamma
+import scipy.stats as stats
 import pandas as pd
+import sys
 from pandas.plotting import table
 
 def read_param(path):
@@ -17,26 +21,28 @@ def convert_param(path):
     param = {}
     input = []
     input = read_param(path)
-    if(len(input) != 9):
+    if (len(input) != 9):
         print('Wrong input value')
         raise Exception
     else:
-        param = {'a' : input[0],
-                 'b' : input[1],
-                 'c' : input[2],
-                 'm' : input[3],
-                 'K1': input[4],
-                 'r' : input[5],
-                 'K2': input[6],
-                 'alpha':input[7], 
-                 'l' : input[8]
-                }
+        param = {
+            'a': input[0],
+            'b': input[1],
+            'c': input[2],
+            'm': input[3],
+            'K1': input[4],
+            'r': input[5],
+            'K2': input[6],
+            'alpha': input[7],
+            'l': input[8]
+        }
     return param
 
 def create_freq(N, param):
     x = np.zeros(N)
-    for i in range(1, N-1):
-        x[i+1] = (param['a']*x[i] + param['b']*(x[i-1] ** 2) + param['c']) % param['m']
+    for i in range(1, N - 1):
+        x[i + 1] = (param['a'] * x[i] + param['b'] *
+                    (x[i - 1]**2) + param['c']) % param['m']
         # x[i+1] = (2*x[i] + 3*(x[i-1]**2) + 4) % 100
     return x
 
@@ -45,25 +51,25 @@ def calc_period(x):
     x2 = x[998]
     T = 2
     x_new = []
-    for i in range(len(x)-3, 1, -1):
-        if(x[i]!=x1):
-            T+=1
+    for i in range(len(x) - 3, 1, -1):
+        if (x[i] != x1):
+            T += 1
         else:
-            if(x[i-1]!=x2):
-                T+=1
+            if (x[i - 1] != x2):
+                T += 1
             else:
                 break
-    if(T > 100):
-        for j in range(i, len(x)-1):
+    if (T > 100):
+        for j in range(i, len(x) - 1):
             x_new.append(x[j])
     return T, x_new
 
 def create_table(df):
-    fig, ax = plot.subplots()
+    fig, ax = plt.subplots()
     fig.patch.set_visible(False)
     ax.axis('off')
     ax.axis('tight')
-    table = ax.table(cellText=df.values, colLabels=df.columns, loc='center')   
+    table = ax.table(cellText=df.values, colLabels=df.columns, loc='center')
     table.auto_set_font_size(False)
     table.set_fontsize(8)
     fig.tight_layout()
@@ -80,42 +86,39 @@ def mo(x):
 def disp(x, m):
     d = 0
     for i in range(len(x)):
-        d += (x[i] - m) ** 2
+        d += (x[i] - m)**2
     d /= (len(x) - 1)
     return d
 
-# функция плотности распределения 
+# функция плотности распределения
 def exp_pdf(x, param):
     l = param['l']
     return (1 / l) * math.exp(-(x / l))
 
-
-# Функция распределения 
+# Функция распределения
 def exp_cdf(x, params):
     l = params['l']
-    vych = (1 / l) * math.exp(-(x / l))
-    return 1 - vych
+    return 1 - math.exp(-(x / l))
 
-# FIX ME
-def inv_func(x, param): 
-    a = exp_cdf(x, param)
+# Обратная функция
+def inv_func(x, param):
+    alpha = x  #exp_cdf(x, param)
     l = param['l']
-    chis = l * (1 - a)
-    chis = np.log(chis)
-    return -l * chis
-
+    return -l * np.log(1 - alpha)
 
 # Функция отрисовки графиков для указанных функций плотности и распределения
-def plot_pdf_and_cdf(pdf, cdf, params, xlims=10, ylims={'pdf':1,'cdf':1}):
+def plot_pdf_and_cdf(pdf, cdf, params, xlims=10, ylims={'pdf': 1, 'cdf': 1}):
     x = np.arange(0, xlims + 0.001, 0.001)
     y_pdf = [pdf(point, params) for point in x]
     y_cdf = [cdf(point, params) for point in x]
 
     fig, sub = plt.subplots(1, 2, figsize=(5, 5))
-    fig.suptitle('Экспоненциальное распределение с параметром \u03C3 = {0}'.format(params['l']))
+    fig.suptitle('Экспоненциальное распределение с параметром λ = {0}'.format(
+        params['l']))
     titles = ['Функция плотности', 'Функция распределения']
 
-    for ax, y, title, kind in zip(sub.flatten(), [y_pdf, y_cdf], titles, ['pdf', 'cdf']):
+    for ax, y, title, kind in zip(sub.flatten(), [y_pdf, y_cdf], titles,
+                                  ['pdf', 'cdf']):
         # Органичение значений осей координат
         ax.set_xlim(0, xlims)
         ax.set_ylim(0, ylims[kind])
@@ -124,7 +127,6 @@ def plot_pdf_and_cdf(pdf, cdf, params, xlims=10, ylims={'pdf':1,'cdf':1}):
         ax.set_title(title)
         ax.plot(x, y, color='green', linewidth=2)
     plt.show()
-
 
 # Функция отрисовки графиков теоретической функции плотности и эмпирической на одном полотне
 #   @pdf - теоретическая функция плотности распределения
@@ -141,7 +143,9 @@ def plot_pdf_theoretical_and_empirical(pdf, params, sample, xlims=10, ylims=1):
 
     # Создание полотна
     fig = plt.figure(figsize=(7, 5))
-    fig.suptitle('Экспоненциальное распределение с параметром \u03C3 = {0}\nДлина последо-вательности N = {1}'.format(params['l'], len(sample)))
+    fig.suptitle(
+        'Экспоненциальное распределение с параметром λ = {0}\nДлина последо-вательности N = {1}'
+        .format(params['l'], len(sample)))
     ax = fig.add_subplot(111)
 
     # Ограничение значений осей
@@ -157,33 +161,140 @@ def plot_pdf_theoretical_and_empirical(pdf, params, sample, xlims=10, ylims=1):
     plt.xticks(bin_edges)
     plt.show()
 
-def create_table(df):
-    fig, ax = plt.subplots()
-    fig.patch.set_visible(False)
-    ax.axis('off')
-    ax.axis('tight')
-    table = ax.table(cellText=df.values, colLabels=df.columns, loc='center')   
-    table.auto_set_font_size(False)
-    table.set_fontsize(8)
-    fig.tight_layout()
+def drawHistogram(ys, spans):
+    plt.hist(ys, spans)
+    plt.show()
+
+# Проверка гипотеза о согласии смоделированной выборки с теоретическим распреде-лением
+#   @sample - выборка
+#   @cdf - теоретическая функция распределения
+#   @param - параметры теоретического распределения
+def chi_square_test(sample, cdf, param):
+    alpha = param['alpha']
+
+    # Длина последовательности
+    n = len(sample)
+
+    # Число интервалов
+    K = int(5 * np.log10(n))
+
+    # Частоты попадания в i-интервал
+    weights, bins_edges = np.histogram(sample,
+                                       bins=K,
+                                       range=(0, np.ceil(max(sample))))
+
+    # Значение статистики
+    S = 0
+    for i in range(K):
+        P = cdf(bins_edges[i + 1], param) - cdf(bins_edges[i], param)
+        S += pow(weights[i] / len(sample) - P, 2) / P
+    S *= n
+
+    # Критическое значение статистики
+    S_crit = stats.chi2.ppf(1 - alpha, K - 1)
+
+    print('Критерий \u03c7\u00b2 Пирсона')
+    print('Значение статистики хи-квадрат: {0}'.format(np.round(S, 3)))
+    print('Критическое значение: {0}'.format(np.round(S_crit, 3)))
+    if S < S_crit:
+        print('Гипотеза о согласии не отвергается\n')
+    else:
+        print('Гипотеза о согласии отвергается\n')
+
+    # Решение через достигнутый уровень значимости
+    p = integrate.quad(lambda x: pow(x, (K - 1) / 2 - 1) * math.exp(-x / 2), S,
+                       math.inf)
+    p /= pow(2, (K - 1) / 2) * gamma((K - 1) / 2)
+
+    print(
+        'Достигнутый уровень значимости: {0}\nУровень значимости: {1}'.format(
+            np.round(p[0], 4), alpha))
+    if p[0] > alpha:
+        print('Гипотеза о согласии не отвергается\n\n')
+    else:
+        print('Гипотеза о согласии отвергается\n\n')
+
+# Непараметрический критерий согласия Омега-Андерсона-Дарлинга
+#   @sample - выборка
+#   @cdf - теоретическая функция распределения
+#   @param - параметры теоретического распределения
+def anderson_darling_test(sample, cdf, param):
+    alpha = param['alpha']
+
+    # Критические значения
+    critical = {
+        0.15: 1.6212,
+        0.1: 1.933,
+        0.05: 2.4924,
+        0.025: 3.0775,
+        0.01: 3.8781
+    }
+
+    n = len(sample)
+    # Упорядочиваем выборку
+    sample = np.sort(sample)
+
+    # Значение статистики
+    S = 0
+    for i in range(n):
+        F = cdf(sample[i], param)
+        if F == 0.0:
+            F = 0.0 + sys.float_info.epsilon
+
+        S += ((2 *
+               (i + 1) - 1) * np.log(F)) / (2 * n) + (1 - (2 * (i + 1) - 1) /
+                                                      (2 * n)) * np.log(1 - F)
+    S = -n - 2 * S
+
+    print('Критерий \u03a9\u00b2-Андерсона-Дарлинга')
+    print('Значение статистики S: {0}'.format(np.round(S, 3)))
+    print('Критическое значение статистики a2(S): {0}'.format(critical[alpha]))
+    if S < critical[alpha]:
+        print('Гипотеза о согласии не отвергается\n')
+    else:
+        print('Гипотеза о согласии отвергается\n')
+
+    # Решение через достигнутый уровень значимости
+    a2 = 0
+    for j in range(10):
+        integral = integrate.quad(
+            lambda y: np.exp(S / (8 * (pow(y, 2) + 1)) - (pow(
+                (4 * j + 1) * np.pi * y, 2)) / (8 * S)), 0, math.inf)[0]
+        a2 += (pow(-1, j) * gamma(j + 0.5) * (4 * j + 1) * np.exp(-pow(
+            (4 * j + 1) * np.pi, 2) / (8 * S))) / (gamma(0.5) *
+                                                   gamma(j + 1)) * integral
+    a2 *= (np.sqrt(2 * np.pi) / S)
+    p = 1 - a2
+    print(
+        'Достигнутый уровень значимости: {0}\nУровень значимости: {1}'.format(
+            np.round(p, 4), alpha))
+    if p > alpha:
+        print('Гипотеза о согласии не отвергается\n\n\n')
+    else:
+        print('Гипотеза о согласии отвергается\n\n\n')
 
 def main():
-    param = convert_param('KM/CompSim_lab4/input.txt')
-    N = 1000
-    x = create_freq(N, param)
-    T, x = calc_period(x)
+    param = convert_param('CompSim_lab4/input.txt')
+    N = 50
+
+    start_time = datetime.now()
+
+    # Генерация равномерно распределенной величины на отрезке (0,1)
+    x_array = create_freq(N, param) / 100
+    # x_array = np.random.rand(N)
+
     # Генерация непрерывной случайной величины методом обратной функции
-    x_array = np.empty(50)
-    for i in range(50):
-        x_array[i] = inv_func(x[i], param)
-    plot_pdf_and_cdf(exp_pdf, exp_cdf, param)
-    # plot_pdf_theoretical_and_empirical(exp_pdf, param, x_array)
+    for i in range(N):
+        x_array[i] = inv_func(x_array[i], param)
+
+    print('Время: ', datetime.now() - start_time)
     # plot_pdf_and_cdf(exp_pdf, exp_cdf, param)
-    # plot_pdf_theoretical_and_empirical(exp_pdf, param, x_array)
-    
-    df = pd.DataFrame()
-    df['Критерий'] = 1
-    create_table(df)
-    plt.show()
+    plot_pdf_theoretical_and_empirical(exp_pdf, param, x_array)
+
+    # Параметрический критерий согласия
+    chi_square_test(x_array, exp_cdf, param)
+
+    # Непараметрический критерий слогласия
+    anderson_darling_test(x_array, exp_cdf, param)
 
 main()
---
+++ CompSim_lab5/main.py
@@ -0,0 +1,364 @@
+import math
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+from scipy.special import gamma
+from scipy.stats import laplace
+from datetime import datetime
+from scipy.optimize import minimize
+import sys
+
+x0 = {
+    (0, 1): {'a': -4, 'b': 10},
+    (3, 2.5): {'a': -4, 'b': 30},
+    (10, 0.4): {'a': 1, 'b': 10}
+}
+
+def read_param(path):
+    input = []
+    with open(path) as file:
+        for line in file:
+            input.append(float(line))
+    return input
+
+def convert_param(path):
+    param = {}
+    input = []
+    input = read_param(path)
+    if(len(input) != 3):
+        print('Wrong input value')
+        raise Exception
+    else:
+        param = {'m' : input[0],
+                 'v' : input[1],
+                 'alpha':input[2],
+                }
+    return param
+
+def create_freq(N, param):
+    x = np.zeros(N)
+    for i in range(1, N - 1):
+        x[i + 1] = (param['a'] * x[i] + param['b'] *
+                    (x[i - 1]**2) + param['c']) % param['m']
+    return x
+
+def calc_period(x):
+    x1 = x[999]
+    x2 = x[998]
+    T = 2
+    x_new = []
+    for i in range(len(x) - 3, 1, -1):
+        if (x[i] != x1):
+            T += 1
+        else:
+            if (x[i - 1] != x2):
+                T += 1
+            else:
+                break
+    if (T > 100):
+        for j in range(i, len(x) - 1):
+            x_new.append(x[j])
+    return T, x_new
+
+def create_table(df):
+    fig, ax = plt.subplots()
+    fig.patch.set_visible(False)
+    ax.axis('off')
+    ax.axis('tight')
+    table = ax.table(cellText=df.values, colLabels=df.columns, loc='center')
+    table.auto_set_font_size(False)
+    table.set_fontsize(8)
+    fig.tight_layout()
+
+# Математическое ожидание
+def mo(x):
+    m = 0
+    for i in range(len(x)):
+        m += x[i]
+    m /= len(x)
+    return m
+
+# Дисперсия
+def disp(x, m):
+    d = 0
+    for i in range(len(x)):
+        d += (x[i] - m)**2
+    d /= (len(x) - 1)
+    return d
+
+# функция плотности распределения
+def lapl_pdf(x, param):
+    m = param['m']
+    v = param['v']
+    return (1 / 2*v) * math.exp(-(m - x)/ v)
+
+
+# Определить границы a,b для заданного уровня точности
+#   @pdf - плотность функции распределения
+#   @cdf - функция распределения
+#   @params - параметры распределения
+def find_bounds(pdf, cdf, params, precision=0.98):
+    mu, var = params
+    # Точка a
+    a = minimize(fun=lambda x: -pdf(x[0], mu, var),
+                   x0=np.array([x0[(mu, var)]['a']]),
+                   tol=1e-6,
+                   constraints={'type': 'eq', 'fun': lambda x: cdf(x[0], mu, var) - (1-precision)/2}).x[0]
+
+    # Точка b
+    b = minimize(fun=lambda x: -pdf(x[0], mu, var),
+                   x0=np.array([x0[(mu, var)]['b']]),
+                   tol=1e-6,
+                   constraints={'type': 'eq', 'fun': lambda x: cdf(x[0], mu, var) - (1+precision)/2}).x[0]
+
+    return [a, b]
+
+
+# Определить M на интервале
+#   @pdf - плотность функции распределения
+#   @params - параметры распределения
+#   @x0 - начальное рпиближение
+def fin_max_pdf(pdf, params, x0):
+    mu, var = params
+
+    x_max = minimize(fun=lambda x: -pdf(x[0], mu, var),
+                   x0=np.array([x0]),
+                   tol=1e-6).x[0]
+
+    return pdf(x_max, mu, var)
+
+
+# Построение графика плотности распределения
+#   @pdf - плотность функции распределения
+#   @params - параметры распределения
+def plot_pdf(pdf, params):
+    mu, var = params
+
+    # Точки графика
+    x = np.arange(x0[(mu, var)]['a'] - 1, x0[(mu, var)]['b'] + 1.001, 0.001)
+    y = [pdf(point, mu, var) for point in x]
+
+    # Создание полотна
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111)
+    fig.suptitle('Плотность распределения Лапласа с параметрами: \u03BC = {0}; \u03C3 = {1}'.format(mu, var))
+
+    # Ограничение значений осей
+    ax.set_xlim(min(x), max(x))
+    ax.set_ylim(-0.5, 1)
+
+    # Cетка
+    ax.grid()
+    ax.plot(x, y, linewidth=3)
+
+    plt.show()
+
+
+# Функция отрисовки графиков теоретической функции плотности и эмпирической на одном по-лотне
+#   @pdf - теоретическая функция плотности распределения
+#   @params - параметры распределения
+#   @sample - выборка
+#   @interval - интервал моделирования (a,b)
+def plot_pdf_theoretical_and_empirical(pdf, params, sample, interval):
+    n = len(sample)
+    mu, var = params
+    a, b = interval[0], interval[1]
+
+    x = np.arange(x0[(mu, var)]['a']-1, x0[(mu, var)]['b']+1.001, 0.001)
+    # Теоретические значения
+    y = [pdf(point, mu, var) for point in x]
+
+    # Число интервалов
+    K = int(5 * np.log10(n))
+
+    # Эмпирические значения
+    weights, bins_edges = np.histogram(sample, bins=K, range=(a, b))
+
+    # Число попаданий переведем в высоты столбца гистограммы
+    weights = np.array(weights) / n
+    for i in range(len(weights)):
+        weights[i] /= (bins_edges[i+1]-bins_edges[i])
+
+    # Создание полотна
+    fig = plt.figure(figsize=(10, 10))
+    fig.suptitle('Распределение Лапласа с параметрами: \u03BC = {0}; \u03C3 = {1}\nДлина по-следовательности N = {2}'\
+                 .format(mu, var, len(sample)))
+
+    ax = fig.add_subplot(111)
+
+    # Ограничение значений осей
+    ax.set_xlim(min(x), max(x))
+    ax.set_ylim(-0.5, 1)
+    # Cетка
+    ax.grid()
+
+    # Вывод эмпирической функции
+    ax.hist(bins_edges[:-1], bins_edges, weights=weights)
+    # Вывод теоретической функции
+    ax.plot(x, y, linewidth=3)
+    # Значения оси Ox
+    plt.xticks(bins_edges)
+    ax.tick_params(axis='x', rotation=90)
+    plt.show()
+
+
+# Проверка гипотеза о согласии смоделированной выборки с теоретическим распределением
+#   @sample - выборка
+#   @cdf - теоретическая функция распределения
+#   @params - параметры теоретического распределения
+#   @interval - интервал моделирования (a,b)
+def chi_square_test(sample, cdf, params, interval, alpha=0.05):
+    # Длина последовательности
+    n = len(sample)
+    mu, v = params
+    # Число интервалов
+    K = int(5 * np.log10(n))
+
+    # Частоты попадания в i-интервал
+    weights, bins_edges = np.histogram(sample,
+                                       bins=K,
+                                       range=(0, np.ceil(max(sample))))
+
+    # Значение статистики
+    S = 0
+    for i in range(K):
+        P = cdf(bins_edges[i + 1], mu, v) - cdf(bins_edges[i], mu, v)
+        S += pow(weights[i] / len(sample) - P, 2) / P
+    S *= n
+
+    # Критическое значение статистики
+    S_crit = stats.chi2.ppf(1 - alpha, K - 1)
+
+    print('Критерий \u03c7\u00b2 Пирсона')
+    print('Значение статистики хи-квадрат: {0}'.format(np.round(S, 3)))
+    print('Критическое значение: {0}'.format(np.round(S_crit, 3)))
+    if S < S_crit:
+        print('Гипотеза о согласии не отвергается\n')
+    else:
+        print('Гипотеза о согласии отвергается\n')
+
+    # Решение через достигнутый уровень значимости
+    p = integrate.quad(lambda x: pow(x, (K - 1) / 2 - 1) * math.exp(-x / 2), S,
+                       math.inf)
+    p /= pow(2, (K - 1) / 2) * gamma((K - 1) / 2)
+
+    print(
+        'Достигнутый уровень значимости: {0}\nУровень значимости: {1}'.format(
+            np.round(p[0], 4), alpha))
+    if p[0] > alpha:
+        print('Гипотеза о согласии не отвергается\n\n')
+    else:
+        print('Гипотеза о согласии отвергается\n\n')
+
+# Вычисление значения I(z)
+def I(z, v):
+    result = 0
+    for k in range(pow(2, 5)):
+        result += pow(z/2, v+2*k)/(gamma(k+1)*gamma(k+v+1))
+    return result
+
+
+# Проверка гипотеезы о согласии с теоретическим распределением по критерию Крамера-Мизеса-Смирнова
+#   @sample - выборка
+#   @cdf - теоретическая функция распределения
+#   @params - параметры теоретического распределения
+def cramer_von_mises_test(sample, cdf, params):
+    n = len(sample)
+    S_critical = 0.4614
+
+    # Сортировка выборки
+    sample = np.sort(sample)
+
+    # Значение статистики
+    S = 1 / (12*n)
+    for i in range(n):
+        if len (params) < 2:
+            S += pow(cdf(sample[i], params[0]) - (2*(i+1)-1)/(2*n), 2)
+        else:
+            S += pow(cdf(sample[i], params[0], params[1]) - (2*(i+1)-1)/(2*n), 2)
+
+
+    print('Критерий \u03C9\u00b2-Крамера-Мизеса-Смирнова')
+    print('Значение статистики: {0}'.format(np.round(S, 3)))
+    print('Критическое значение: {0}'.format(S_critical))
+    if S < S_critical:
+        print('Гипотеза о согласии не отвергается\n')
+    else:
+        print('Гипотеза о согласии отвергается\n')
+
+    # Решение через достигнутый уровень значимости
+    a1 = 0
+    for j in range(pow(2, 6)):
+        temp = (gamma(j+0.5)*np.sqrt(4*j+1))/(gamma(0.5)*gamma(j+1))
+        temp *= np.exp(-pow(4*j+1, 2)/(16*S))
+        temp *= (I(pow(4*j+1, 2)/(16*S), -0.25) - I(pow(4*j+1, 2)/(16*S), 0.25))
+        a1 += temp
+    a1 /= np.sqrt(2*S)
+
+    p = 1 - a1
+
+    print('Достигнутый уровень значимости: {0}\nУровень значимости: {1}'.format(np.round(p, 4), 0.05))
+    if p > 0.05:
+        print('Гипотеза о согласии не отвергается\n\n\n')
+    else:
+        print('Гипотеза о согласии отвергается\n\n\n')
+
+
+
+def main():
+    np.random.seed(5)
+
+    output = open('output.txt', 'w', encoding='utf-8')
+    param = convert_param('KM\CompSim_lab5\input.txt')
+
+    # Параметры распределения
+    mu = param['m']
+    v = param['v']
+
+    # Выбор границ интервала
+    a, b = find_bounds(pdf=laplace.pdf, cdf=laplace.cdf, params=[mu, v], precision=0.98)
+
+    # Выбор множества точек
+    M = fin_max_pdf(pdf=laplace.pdf, params=[mu, v], x0=a)
+
+    plot_pdf(pdf=laplace.pdf , params=[mu, v])
+
+    print('Интервал моделирования: a = {0}, b = {1}\nM = {2}\n'.format(np.round(a, 3), np.round(b, 3), np.round(M, 3)))
+    n = 50
+
+    # Генерирование равномерно распределенной случайной величины
+    arr = np.random.uniform(0, 1, 10000)
+
+    x_array = []
+
+    # Начальная точка замера времени моделирования
+    while len(x_array) < n:
+        # Берем пару равномерно распределенных чисел
+        alpha1, alpha2 = arr[0], arr[1]
+
+        # Находим координаты точки
+        x = a + alpha1 * (b - a)
+        y = alpha2 * M
+
+        # Принимаем или отвергаем новую точку
+        if y < laplace.pdf(x, mu, v):
+            x_array.append(x)
+
+        # Отбрасываем использованные величины
+        arr = np.delete(arr, [0, 1])
+
+    print('N = {0}\nX = {{'.format(n, x_array))
+    for i in range(n):
+        print(np.round(x_array[i], 3))
+    print('}')
+    print('Равномерно распределенных величин потребовалось: {0}\n'.format(10000-len(arr)))
+
+    plot_pdf_theoretical_and_empirical(pdf=laplace.pdf, params=[mu, v], sample=x_array, interval=[a,b])
+
+    chi_square_test(sample=x_array, cdf=laplace.cdf, params=[mu, v], interval=[a,b])
+
+    cramer_von_mises_test(sample=x_array, cdf=laplace.cdf, params=[mu, v])
+
+
+main()
